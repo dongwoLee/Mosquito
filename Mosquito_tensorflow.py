c3917b27cbc8429de890cf599e3446f4bec9f3c0
dongwoLee <dongwoo4553@gmail.com>
2017-03-05 20:06:17 +0900

    mosquito tensorflow

--- Mosquito_tensorflow.py
+++ Mosquito_tensorflow.py
@@ -12,9 +12,19 @@ def makeList(fList):
 
     factor = []
     for i in range(len(fList)):
-        factor.append(fList[i][1])
+        factor.append(float(fList[i][0]))
 
-    return factor
+    return (factor)
 
 if __name__ == '__main__':
-    print (len(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/mosquito_result.csv")))
+    humidityList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/humidity_result.csv")))
+    temperatureMaxList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/temperature.max_result.csv")))
+    temperatureMinList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/temperature.min_result.csv")))
+    temperatureAvgList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/temperature.avg_result.csv")))
+    mosquitoList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/mosquito_result.csv")))
+
+    print (humidityList)
+    print (temperatureMaxList)
+    print (temperatureAvgList)
+    print (temperatureMinList)
+    print (mosquitoList)
