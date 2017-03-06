52d01d0a3ced8421ab456f5b2759aa0d97c35b8e
dongwoLee <dongwoo4553@gmail.com>
2017-03-06 20:30:09 +0900

    tensorflow mosquito

--- Mosquito_tensorflow.py
+++ Mosquito_tensorflow.py
@@ -18,23 +18,25 @@ def makeList(fList):
 
 if __name__ == '__main__':
     humidityList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/humidity_result.csv")))
+    rainFallList = makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/rainfall_result.csv"))
     temperatureMaxList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/temperature.max_result.csv")))
     temperatureMinList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/temperature.min_result.csv")))
     temperatureAvgList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/temperature.avg_result.csv")))
     mosquitoList = (makeList(readCsvToList("C:/Users/dw/Desktop/Mosquito/Mosquito/mosquito_result.csv")))
 
-    W1= tf.Variable(tf.random_uniform([1],-1.0,1.0))
-    W2 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+    W1 = tf.Variable(tf.random_uniform([1],-1.0,1.0))
+    W2 = tf.Variable(tf.random_uniform([1],-1.0,1.0))
     W3 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
     W4 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+    W5 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
     b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
-    hypothesis = W1 * humidityList + W2 * temperatureMaxList + W3 * temperatureAvgList + W4 * temperatureMinList + b
+    hypothesis = W1 * humidityList + W2 * rainFallList + W3 * temperatureMaxList + W4 * temperatureAvgList + W5 * temperatureMinList + b
 
     cost = tf.reduce_mean(tf.square(hypothesis-mosquitoList))
 
-    learning_rate = 0.01
+    learning_rate = 0.05
 
     optimizer = tf.train.AdamOptimizer(learning_rate)
     train = optimizer.minimize(cost)
@@ -46,4 +48,6 @@ if __name__ == '__main__':
     for step in range(100001):
         sess.run(train)
         if step%100 == 0:
-            print (step,sess.run(cost),sess.run(W1),sess.run(W2),sess.run(W3),sess.run(W4),sess.run(b))
+            print (step,sess.run(cost),sess.run(W1),sess.run(W2),sess.run(W3),sess.run(W4),sess.run(W5),sess.run(b))
+
+
